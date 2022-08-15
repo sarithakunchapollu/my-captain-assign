f69059d5b5c234b0e61f1d244819475f724a0efc
sarithakunchapollu <111043979+sarithakunchapollu@users.noreply.github.com>
2022-08-15 17:46:48 +0530

    Add files via upload

---
+++ main (1task 2nd program).py
@@ -0,0 +1,6 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+filename =input("input the file name is:saritha")
+file_extension=filename.split(".")
+print("the extension of the file is."+reprint(fileextension(".")))
