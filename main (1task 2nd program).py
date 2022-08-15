a0549907ba805021f913b1c5805d4e1821c93e5d
sarithakunchapollu <111043979+sarithakunchapollu@users.noreply.github.com>
2022-08-15 18:26:02 +0530

    Delete main (1task 2nd program).py

--- main (1task 2nd program).py
+++
@@ -1,6 +0,0 @@
-
-# Online Python - IDE, Editor, Compiler, Interpreter
-
-filename =input("input the file name is:saritha")
-file_extension=filename.split(".")
-print("the extension of the file is."+reprint(fileextension(".")))
